0fe08818405c740bb49d53a21c784df884d3e01b
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2022-04-18 15:06:43 +0900

    feat: add screens

---
+++ screens/FeedsScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+const FeedsScreen = () => {
+  return <View style={styles.block} />;
+};
+
+const styles = StyleSheet.create({
+  block: {},
+});
+
+export default FeedsScreen;
